7a8bc08c28bcd23fdc7a524323790fa41f11e05b
ericslpenner-source <ericslpenner@gmail.com>
2026-03-14 11:56:43 -0400

    modified:   portfolio.html 	modified:   portfolio.js 	modified:   style.css
    implemented the dog api as random "portfolio photos"

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,17 @@
-//fetch(`https://pokeapi.co/api/v2/pokemon/${randomID}`);
+let portfolioImages = [];
+let portfolioGallery = document.getElementById("portfolioGallery");
+getImages();
+function getImages(){
+    for(let i = 0; i < 10; i++)
+    {
+        portfolioImages[i] = document.createElement("img");
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then((response) =>{
+            return response.json();
+        })
+        .then((data) =>{
+        portfolioImages[i].src = data.message;
+        })
+        portfolioGallery.appendChild(portfolioImages[i]);
+    }
+}
